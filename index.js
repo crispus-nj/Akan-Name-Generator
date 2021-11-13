27d5ef95fe85200ac2ee713a71b333e87c9898e6
engineer237 <crispus.muthiora@student.moringaschool.com>
2021-11-13 18:55:12 +0300

    added margin-bottom to the form container. Adjusted the typo error in the select gender which solved the logical error

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ let dayOfTheWeek = [
     "Friday",
     "Saturday",
 ];
-let maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-
 let femaleAkanNames = [
     "Akosua",
     "Adwoa",
@@ -19,40 +17,44 @@ let femaleAkanNames = [
     "Afua",
     "Ama",
 ];
-
-// let maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-
+let maleAkanNames = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+];
 // answer section
-let user_answer = document.querySelector(".user-answer")
+let user_answer = document.querySelector(".user-answer");
 // console.log(user_answer)
 let buttonOnsubmit = document.getElementById("btn-submit");
 buttonOnsubmit.addEventListener("click", (e) => {
     e.preventDefault();
-    generateName(e)
+    generateName(e);
 });
-
 
 generateName = (e) => {
     let name = document.getElementById("name").value;
-let dateOfBirth = document.getElementById("date").value;
-let gender = document.getElementById("gender").value;
-let dateNumber = new Date(dateOfBirth).getDay()
-    e.preventDefault()
+    let dateOfBirth = document.getElementById("date").value;
+    let gender = document.getElementById("gender").value;
+    let dateNumber = new Date(dateOfBirth).getDay();
+    e.preventDefault();
     // user_answer.innerHTML()
     // console.log(name, dateNumber, gender);
-    if (gender == "male") {
-       return user_answer.innerHTML = `
+    if (gender === "male") {
+        return (user_answer.innerHTML = `
         <div>
             <h5><strong>Name</strong>: ${name} </h5>
             <h5><strong>Date Of Birth</strong>:${dateOfBirth}</h5>
             <h5><strong>Gender</strong>: ${gender} </h5>
             <h5><strong>Day of the week</strong>: ${dayOfTheWeek[dateNumber]}</h5>
             <h5><strong>Akan Name</strong>:${maleAkanNames[dateNumber]}</h5>
-         </div>`
-    //  console.log(`Your name is ${name} and you were born on ${dayOfTheWeek[dateNumber]} and your akan name is ${maleAkanNames[dateNumber]}`)
-
-    }else {
-      return  user_answer.innerHTML = `
+         </div>`);
+        //  console.log(`Your name is ${name} and you were born on ${dayOfTheWeek[dateNumber]} and your akan name is ${maleAkanNames[dateNumber]}`)
+    } else{
+        return (user_answer.innerHTML = `
        <div>
             <h5><strong>Name</strong>: ${name} </h5>
             <h5><strong>Date Of Birth</strong>:${dateOfBirth}</h5>
@@ -60,10 +62,7 @@ let dateNumber = new Date(dateOfBirth).getDay()
             <h5><strong>Day of the week</strong>: ${dayOfTheWeek[dateNumber]}</h5>
             <h5><strong>Akan Name</strong>:${femaleAkanNames[dateNumber]}</h5>
      </div>
-       `        
-    //    console.log(`Your name is ${name} and you were born on ${dayOfTheWeek[dateNumber]} and your akan name is ${femaleAkanNames[dateNumber]}`)
+       `);
+        //    console.log(`Your name is ${name} and you were born on ${dayOfTheWeek[dateNumber]} and your akan name is ${femaleAkanNames[dateNumber]}`)
     }
-    
-
 };
-
