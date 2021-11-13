5d0a36a13a8e67ad86269c4743a583e05b77776b
engineer237 <crispus.muthiora@student.moringaschool.com>
2021-11-13 14:24:28 +0300

    created the logic of filtering out the date and gender of the user. commented bootstrap links which performs js functionality since they are not needed

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
 // data in array
-let femaleAkanNames = ["Akosua", "Abenaa", "Kwabena", "Akua", "Yaa", "Afua", "Ama"]
-let makeAkanName = ["Kwasi", "Kwadwo", "kwaku", "Yaw", "Kofi", "Kwame"]
-let dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"]
+let femaleAkanNames = [
+    "Akosua",
+    "Abenaa",
+    "Kwabena",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+];
+let makeAkanName = ["Kwasi", "Kwadwo", "kwaku", "Yaw", "Kofi", "Kwame"];
+let dayOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednsday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+let buttonOnsubmit = document.getElementById("btn-submit");
+buttonOnsubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    generateName(e)
+});
+
+
+generateName = (e) => {
+    e.preventDefault()
+    let name = document.getElementById("name").value;
+    let dateOfBirth = document.getElementById("date").value;
+    let gender = document.getElementById("gender").value;
+    let dateNumber = new Date(dateOfBirth).getDay()
+    // console.log(name, dateNumber, gender);
+    if(gender == "male"){
+        console.log(`Your name is ${name} and you were born on ${dayOfTheWeek[dateNumber]} and your akan name is ${makeAkanName[dateNumber]}`)
+    }else{
+        console.log(`Your name is ${name} and you were born on ${dayOfTheWeek[dateNumber]} and your akan name is ${femaleAkanNames[dateNumber]}`)
+    }
+
+};
+
