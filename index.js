7e47cb83591830ae202df3a1c1c68b0021cfb574
engineer237 <crispus.muthiora@student.moringaschool.com>
2021-11-13 13:54:13 +0300

    create all data in form of arrays in the index.js file

---
+++ index.js
@@ -0,0 +1,4 @@
+// data in array
+let femaleAkanNames = ["Akosua", "Abenaa", "Kwabena", "Akua", "Yaa", "Afua", "Ama"]
+let makeAkanName = ["Kwasi", "Kwadwo", "kwaku", "Yaw", "Kofi", "Kwame"]
+let dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"]
