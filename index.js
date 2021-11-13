716376b5a67fd136c17f0389aea6db3077e50366
engineer237 <crispus.muthiora@student.moringaschool.com>
2021-11-13 18:14:41 +0300

    added boldness in the card menu section, restructured the index.js file

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 // data in array
-let femaleAkanNames = [
-    "Akosua",
-    "Adwoa",
-    "Abenaa",
-    "Akua",
-    "Yaa",
-    "Afua",
-    "Ama",
-];
-let maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let dayOfTheWeek = [
     "Sunday",
     "Monday",
@@ -18,7 +8,22 @@ let dayOfTheWeek = [
     "Friday",
     "Saturday",
 ];
-let user_answer = document.querySelector(".user-answer") 
+let maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+let femaleAkanNames = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+];
+
+// let maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+// answer section
+let user_answer = document.querySelector(".user-answer")
 // console.log(user_answer)
 let buttonOnsubmit = document.getElementById("btn-submit");
 buttonOnsubmit.addEventListener("click", (e) => {
@@ -28,33 +33,37 @@ buttonOnsubmit.addEventListener("click", (e) => {
 
 
 generateName = (e) => {
-    e.preventDefault()
     let name = document.getElementById("name").value;
-    let dateOfBirth = document.getElementById("date").value;
-    let gender = document.getElementById("gender").value;
-    let dateNumber = new Date(dateOfBirth).getDay()
+let dateOfBirth = document.getElementById("date").value;
+let gender = document.getElementById("gender").value;
+let dateNumber = new Date(dateOfBirth).getDay()
+    e.preventDefault()
     // user_answer.innerHTML()
     // console.log(name, dateNumber, gender);
-    if(gender == "male"){
-        
-      user_answer.innerHTML = `<div>
-      <h4>Name: ${name} </h4>
-      <h4>Date Of Birth: ${dateOfBirth}</h4>
-      <h4>Gender: ${gender} </h4>
-      <h4><strong>Day of the week born</strong>: ${dayOfTheWeek[dateNumber]}</h4>
-      <h4><strong>Akan Name</strong>: ${maleAkanNames[dateNumber]}</h4>
+    if (gender == "male") {
+       return user_answer.innerHTML = `
+        <div>
+            <h5><strong>Name</strong>: ${name} </h5>
+            <h5><strong>Date Of Birth</strong>:${dateOfBirth}</h5>
+            <h5><strong>Gender</strong>: ${gender} </h5>
+            <h5><strong>Day of the week</strong>: ${dayOfTheWeek[dateNumber]}</h5>
+            <h5><strong>Akan Name</strong>:${maleAkanNames[dateNumber]}</h5>
+         </div>`
+    //  console.log(`Your name is ${name} and you were born on ${dayOfTheWeek[dateNumber]} and your akan name is ${maleAkanNames[dateNumber]}`)
 
-  </div>`
-    }else{
-       user_answer.innerHTML = `<div>
-       <h4>Name: ${name} </h4>
-       <h4>Date Of Birth: ${dateOfBirth}</h4>
-       <h4>Gender: ${gender} </h4>
-       <h4><strong>Day of the week born</strong>: ${dayOfTheWeek[dateNumber]}</h4>
-       <h4><strong>Akan Name</strong>: ${maleAkanNames[dateNumber]}</h4>
-
-   </div>`        // console.log(`Your name is ${name} and you were born on ${dayOfTheWeek[dateNumber]} and your akan name is ${femaleAkanNames[dateNumber]}`)
+    }else {
+      return  user_answer.innerHTML = `
+       <div>
+            <h5><strong>Name</strong>: ${name} </h5>
+            <h5><strong>Date Of Birth</strong>:${dateOfBirth}</h5>
+            <h5><strong>Gender</strong>: ${gender} </h5>
+            <h5><strong>Day of the week</strong>: ${dayOfTheWeek[dateNumber]}</h5>
+            <h5><strong>Akan Name</strong>:${femaleAkanNames[dateNumber]}</h5>
+     </div>
+       `        
+    //    console.log(`Your name is ${name} and you were born on ${dayOfTheWeek[dateNumber]} and your akan name is ${femaleAkanNames[dateNumber]}`)
     }
+    
 
 };
 
